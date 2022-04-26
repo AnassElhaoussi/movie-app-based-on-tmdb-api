f769a9e5d54e040b929eb72ab5cee14609fca4c5
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-04-26 23:31:22 +0200

    Making the user able to search for any movie he wants using search method

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
-const API_KEY =  'dfaa2effcf2a2d78c559c0278db75f00'
+const API_KEY =  'api_key=dfaa2effcf2a2d78c559c0278db75f00'
 const BASE_URL = 'https://api.themoviedb.org/3'
-const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`
+const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/'
 const mainSection = document.querySelector('.main-section')
-const inputField = document.querySelector(".navigation input")
+const inputField = document.querySelector(".inputField")
+const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}`
 
 
 
@@ -16,8 +17,8 @@ const fetchData = (api) => {
         displayMovies(data.results)
     })
 }
-
 fetchData(API_URL)
+
 
 
 
@@ -53,18 +54,14 @@ const displayMovies = cards => {
 }
 
 inputField.addEventListener('keyup', e => {
+
     const {value} = e.target
-    const movieName = document.querySelectorAll(".main-text h3")
 
-    movieName.forEach(name => {
-        if(name.innerHTML.toLowerCase().includes(value.toLowerCase())){
-            name.parentElement.parentElement.parentElement.style.display = "block"            
-        } else {
-            name.parentElement.parentElement.parentElement.style.display = "none"
-        }
-
-    })
-
+    if(value){
+        fetchData(SEARCH_URL+"&query="+value)
+    } else {
+        fetchData(API_URL)
+    }
 
 })
 
